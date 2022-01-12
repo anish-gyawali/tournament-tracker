d124ba7d8484a9af3e8d84a4e3520dde34077556
Anish Gyawali <anish.gyawali@selu.edu>
2022-01-12 12:58:20 -0600

    Added model class library

---
+++ TournamentTracker/TrackerLibrary/PrizeModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class PrizeModel
+    {
+        /// <summary>
+        /// Represents the place number.
+        /// </summary>
+        public int PlaceNumber { get; set; }
+
+        /// <summary>
+        /// Represents the place name.
+        /// </summary>
+        public string PlaceName { get; set; }
+
+        /// <summary>
+        /// Represnts the prize amount for the team.
+        /// </summary>
+        public decimal PrizeAmount { get; set; }
+
+        /// <summary>
+        /// Represents the prize percentage for the team.
+        /// </summary>
+        public double PrizePercentage { get; set; }
+    }
+}
---
+++ TournamentTracker/TrackerLibrary/TournamentModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TournamentModel
+    {
+        /// <summary>
+        /// Represents the name of tournament.
+        /// </summary>
+        public string TournamentName { get; set; }
+
+        /// <summary>
+        /// Represents the entry fee for the tournament
+        /// </summary>
+        public decimal EntryFee { get; set; }
+
+        /// <summary>
+        /// Represents the list of entered teams
+        /// </summary>
+        public List<TeamModel> EnteredTeams { get; set; }= new List<TeamModel>();
+
+        /// <summary>
+        /// Represent the prizes for the teams based on their place.
+        /// </summary>
+        public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
+
+        /// <summary>
+        /// Represent thr round of tournament.
+        /// </summary>
+        public List<List<MatchupModel>> Rounds { get; set; } = new List<List<MatchupModel>>();
+    }
+}
